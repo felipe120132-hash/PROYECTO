3cea16a2843d37035f1c7143444d8fba666c830c
felipe120132-hash <felipe120132@gmail.com>
2026-05-22 20:54:49 -0300

    definitivadsdsds

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
   credentials: true
 }));
 
